483abca9b48187049e4e3f2804c73065ca13e7cb
cicloid <cicloid@42fu.com>
2015-11-23 03:05:02 -0600

    Almost all examples working for BTree

--- ruby/btree.rb
+++ ruby/btree.rb
@@ -1,14 +1,119 @@
-class BTree
+class Node
+  attr_accessor :keys, :children, :values, :leaf
 
-  def initialize
-    @root = Node.new
+  def initialize(degree)
+    @keys = Array.new
+    @children = Array.new
+    @values = Array.new
+    @degree = degree
+  end
+
+  def leaf?
+    @children.size === 0
+  end
+
+  def children
+    @children
+  end
+
+  def size
+    @keys.size || 0
+  end
+
+  def full?
+    size >= 2 * @degree - 1
+  end
+
+  def size
+    @keys.size
+  end
+
+  def search(search_key)
+    i = 0
+    while (i <= (size - 1 || 0) &&
+        search_key > @keys.fetch(i, 0))
+      i += 1
+    end
+
+    if search_key === @keys[i]
+      return @values[i]
+    elsif leaf?
+      return
+    end
+    @children[i].search(search_key)
+  end
+
+  def insert(insert_key, value)
+    i = 0
+    while i <= (size - 1 || 0) &&
+        insert_key > @keys.fetch(i, 0)
+      i += 1
+    end
+    if leaf?
+      @keys.insert(i, insert_key)
+      @values.insert(i, value)
+    else
+      child = @children[i]
+
+      if child.full?
+        split(i)
+      end
+
+      child.insert(insert_key, value)
+    end
+
+  end
+
+  def split(i)
+    new_child = Node.new(@degree)
+
+    middle = @degree - 1
+
+    new_child.keys << split_child.keys.slice!(middle + 1) || []
+    new_child.values << split_child.values.slice!(middle + 1) || []
+    new_child.add_children split_child.children.slice!(middle + 1)
+
+    @children.insert(i + 1, new_child)
+
+    @keys.insert(i, split_child.keys[middle])
+    @values.insert(i, split_child.values[middle])
+
+    split_child.keys.delete_at(middle)
+    split_child.values.delete_at(middle)
+  end
+
+  def add_children(children)
+    @children << children
+    @children.compact!
+  end
+
+
+  def inspect
+    "{#{@keys}::#{@children.each(&:inspect)}}"
+  end
+
+  def to_s
+    @keys.join(', ')
+  end
+
+end
+
+
+class BTree
+  attr_accessor :root, :degree
+
+  def initialize(degree = 2)
+    @degree = degree
+    @root = Node.new(@degree)
   end
 
   def insert(key, value)
-    if @root.full?
-
+    root = @root
+    if root.full?
+      @root = Node.new(@degree)
+      @root.add_children root
+      @root.split(0)
     end
-
     @root.insert(key, value)
   end
 
@@ -16,58 +121,24 @@ class BTree
     @root.search(key)
   end
 
-
-  def self.degree
-    2
+  def size
+    @root.size
   end
 
-end
-
-class Node
-  def initialize
-    @children = []
-    @keys = []
-    @values = []
+  def inspect
+    @root.inspect
   end
 
-  def search(search_key)
-    @keys.each do |key|
-      if search_key === key
-        return @values[key]
-      elsif is_leaf?
-        return
-      end
-
-      @children[key].search(search_key)
-    end
+  def keys
+    @root.keys
   end
 
-  def full?
-    @keys.size >= 2 * BinaryTree.degree - 1
+  def children
+    @root.children
   end
 
-  def insert(insert_key, value)
-    @keys.each do |key|
-      if is_leaf?
-        @keys.insert(key.index, insert_key)
-        @values.insert(key.index, value)
-      else
-        child = @children[key.index]
-
-        if child.full?
-          split(i)
-        end
-
-        child.insert(key, value)
-      end
-      i += 1
-    end
-
+  def values
+    @root.values
   end
-
-  def is_leaf?
-    @children.empty?
-  end
-
 
 end
--- ruby/btree_spec.rb
+++ ruby/btree_spec.rb
@@ -1,4 +1,7 @@
+require 'rubygems'
+require 'bundler/setup'
 require 'minitest/autorun'
+require 'pry-byebug'
 require './btree'
 
 
@@ -6,16 +9,106 @@ require './btree'
 # http://www.cs.utexas.edu/users/djimenez/utsa/cs3343/lecture17.html
 #
 describe BTree do
+
   before do
-    @btree = BTree.new
+    @btree = BTree.new(2)
   end
 
   it 'insert 5' do
     @btree.insert(5, 5)
 
-    puts @btree.inspect
     @btree.search(5).must_equal 5
+    @btree.keys.must_equal [5]
   end
+
+  it 'insert 9' do
+    @btree.insert(5, 5)
+    @btree.insert(9, 9)
+
+    @btree.keys.must_equal [5, 9]
+  end
+
+  it 'insert 3' do
+    @btree.insert(5, 5)
+    @btree.insert(9, 9)
+    @btree.insert(3, 3)
+
+
+    @btree.keys.must_equal [3, 5, 9]
+  end
+
+  it 'insert 7' do
+    @btree.insert(5, 5)
+    @btree.insert(9, 9)
+    @btree.insert(3, 3)
+    @btree.insert(7, 7)
+
+
+    @btree.inspect.must_equal "{[5]::[{[3]::[]}, {[7, 9]::[]}]}"
+  end
+
+  it 'insert 1' do
+    @btree.insert(5, 5)
+    @btree.insert(9, 9)
+    @btree.insert(3, 3)
+    @btree.insert(7, 7)
+    @btree.insert(1, 1)
+
+    @btree.inspect.must_equal "{[5]::[{[1, 3]::[]}, {[7, 9]::[]}]}"
+  end
+
+  it 'insert 2' do
+    @btree.insert(5, 5)
+    @btree.insert(9, 9)
+    @btree.insert(3, 3)
+    @btree.insert(7, 7)
+    @btree.insert(1, 1)
+    @btree.insert(2, 2)
+
+    @btree.inspect.must_equal "{[5]::[{[1, 2, 3]::[]}, {[7, 9]::[]}]}"
+  end
+
+  it 'insert 8' do
+    @btree.insert(5, 5)
+    @btree.insert(9, 9)
+    @btree.insert(3, 3)
+    @btree.insert(7, 7)
+    @btree.insert(1, 1)
+    @btree.insert(2, 2)
+    @btree.insert(8, 8)
+
+    @btree.inspect.must_equal "{[5]::[{[1, 2, 3]::[]}, {[7, 8, 9]::[]}]}"
+  end
+
+  it 'insert 6' do
+    @btree.insert(5, 5)
+    @btree.insert(9, 9)
+    @btree.insert(3, 3)
+    @btree.insert(7, 7)
+    @btree.insert(1, 1)
+    @btree.insert(2, 2)
+    @btree.insert(8, 8)
+    @btree.insert(6, 6)
+
+    @btree.keys.must_equal [5, 8]
+    @btree.inspect.must_equal "{[5, 8]::[{[1, 2, 3]::[]}, {[6, 7]::[]}, {[9]::[]}]}"
+  end
+
+  it 'insert 0' do
+    @btree.insert(5, 5)
+    @btree.insert(9, 9)
+    @btree.insert(3, 3)
+    @btree.insert(7, 7)
+    @btree.insert(1, 1)
+    @btree.insert(2, 2)
+    @btree.insert(8, 8)
+    @btree.insert(6, 6)
+    @btree.insert(0, 0)
+
+    @btree.keys.must_equal [2,5,8]
+    @btree.inspect.must_equal "{[2, 5, 8]::[{[0, 1]::[]}, {[3]::[]}, {[6, 7]::[]}, {[9]::[]}]}"
+  end
+
 
 
 end
